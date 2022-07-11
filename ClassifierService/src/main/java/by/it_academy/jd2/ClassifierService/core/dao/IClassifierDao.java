ab8823c97d6ab122f3a8dcc9e7a6d55f390e3c1d
Ilya Papruga <ilya.papruga@gmail.com>
2022-07-11 14:49:12 +0300

    del temp classes

--- ClassifierService/src/main/java/by/it_academy/jd2/ClassifierService/core/dao/IClassifierDao.java
+++
@@ -1,9 +0,0 @@
-package by.it_academy.jd2.ClassifierService.core.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IClassifierDao <T, ID> extends JpaRepository <T,ID> {
-
-}
