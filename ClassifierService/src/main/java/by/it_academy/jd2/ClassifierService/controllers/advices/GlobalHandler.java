65ee58a1f6d9083e04c8a15a75ed00f7db3a3b80
Ilya Papruga <ilya.papruga@gmail.com>
2022-07-13 00:11:40 +0300

    add multiply error handle

--- ClassifierService/src/main/java/by/it_academy/jd2/ClassifierService/controllers/advices/GlobalHandler.java
+++ ClassifierService/src/main/java/by/it_academy/jd2/ClassifierService/controllers/advices/GlobalHandler.java
@@ -1,11 +1,16 @@
 package by.it_academy.jd2.ClassifierService.controllers.advices;
 
+import by.it_academy.jd2.ClassifierService.core.dto.error.FieldError;
+import by.it_academy.jd2.ClassifierService.core.dto.error.MultiplyError;
 import by.it_academy.jd2.ClassifierService.core.dto.error.SingleError;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,9 +23,26 @@ public class GlobalHandler {
 
         List<SingleError> errorResponseList = new ArrayList<>();
 
-        errorResponseList.add(new SingleError(e.getClass().getSimpleName(),e.getMessage()));
+        errorResponseList.add(new SingleError(e.getMessage()));
 
         return errorResponseList;
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MultiplyError handle(ConstraintViolationException e) {
+        List<FieldError> fieldError = new ArrayList<>();
+
+        String field = null;
+
+        for (ConstraintViolation constraintViolation : e.getConstraintViolations()) {
+
+            for (Path.Node node : constraintViolation.getPropertyPath()) {
+                field = node.getName();
+            }
+            fieldError.add(new FieldError(constraintViolation.getMessage(), field));
+        }
+        return new MultiplyError(fieldError);
     }
 
 
--- EventConcertService/src/main/java/by/it_academy/jd2/EventConcertService/controllers/advices/GlobalHandler.java
+++ EventConcertService/src/main/java/by/it_academy/jd2/EventConcertService/controllers/advices/GlobalHandler.java
@@ -1,27 +1,48 @@
 package by.it_academy.jd2.EventConcertService.controllers.advices;
 
+import by.it_academy.jd2.EventConcertService.core.dto.error.FieldError;
+import by.it_academy.jd2.EventConcertService.core.dto.error.MultiplyError;
 import by.it_academy.jd2.EventConcertService.core.dto.error.SingleError;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestControllerAdvice
 public class GlobalHandler {
 
-    @ExceptionHandler (IllegalArgumentException.class)
+    @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public List<SingleError> handle (IllegalArgumentException e){
+    public List<SingleError> handle(IllegalArgumentException e) {
 
         List<SingleError> errorResponseList = new ArrayList<>();
 
-        errorResponseList.add(new SingleError(e.getClass().getSimpleName(),e.getMessage()));
+        errorResponseList.add(new SingleError(e.getMessage()));
 
         return errorResponseList;
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MultiplyError handle(ConstraintViolationException e) {
+        List<FieldError> fieldError = new ArrayList<>();
 
+        String field = null;
+
+        for (ConstraintViolation constraintViolation : e.getConstraintViolations()) {
+
+            for (Path.Node node : constraintViolation.getPropertyPath()) {
+                field = node.getName();
+            }
+            fieldError.add(new FieldError(constraintViolation.getMessage(), field));
+        }
+        return new MultiplyError(fieldError);
     }
+
+}
