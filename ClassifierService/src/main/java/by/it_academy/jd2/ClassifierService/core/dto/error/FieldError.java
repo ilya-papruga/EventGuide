64b70471355edd2becda055181dc93fd3ce1640b
Ilya Papruga <ilya.papruga@gmail.com>
2022-07-13 00:24:22 +0300

    add multiply error

---
+++ ClassifierService/src/main/java/by/it_academy/jd2/ClassifierService/core/dto/error/FieldError.java
@@ -0,0 +1,31 @@
+package by.it_academy.jd2.ClassifierService.core.dto.error;
+
+public class FieldError {
+
+    private String message;
+    private String filed;
+
+    public FieldError() {
+    }
+
+    public FieldError(String message, String filed) {
+        this.message = message;
+        this.filed = filed;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getFiled() {
+        return filed;
+    }
+
+    public void setFiled(String filed) {
+        this.filed = filed;
+    }
+}
