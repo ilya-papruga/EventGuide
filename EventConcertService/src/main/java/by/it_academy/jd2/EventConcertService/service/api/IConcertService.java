93025a5673c881d3923933dc4930a4affd5e95ec
Ilya Papruga <ilya.papruga@gmail.com>
2022-07-21 02:38:10 +0300

    refactoring

--- EventConcertService/src/main/java/by/it_academy/jd2/EventConcertService/service/api/IConcertService.java
+++ EventConcertService/src/main/java/by/it_academy/jd2/EventConcertService/service/api/IConcertService.java
@@ -1,6 +1,6 @@
 package by.it_academy.jd2.EventConcertService.service.api;
 
-import by.it_academy.jd2.EventConcertService.core.dto.flim.ConcertCreateUpdate;
+import by.it_academy.jd2.EventConcertService.core.dto.concert.ConcertCreateUpdate;
 import by.it_academy.jd2.EventConcertService.core.entity.Concert;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +9,6 @@ import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 @Validated
 public interface IConcertService {
