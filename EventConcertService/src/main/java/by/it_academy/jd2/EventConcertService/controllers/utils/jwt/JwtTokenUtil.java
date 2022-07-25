f60047b7a68359ad28d599914af35bcdbaad01cc
Ilya Papruga <ilya.papruga@gmail.com>
2022-07-25 16:16:59 +0300

    add security

---
+++ EventConcertService/src/main/java/by/it_academy/jd2/EventConcertService/controllers/utils/jwt/JwtTokenUtil.java
@@ -0,0 +1,89 @@
+package by.it_academy.jd2.EventConcertService.controllers.utils.jwt;
+
+import io.jsonwebtoken.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class JwtTokenUtil {
+
+    private static final String jwtSecret = "NDQ1ZjAzNjQtMzViZi00MDRjLTljZjQtNjNjYWIyZTU5ZDYw";
+    private static final String jwtIssuer = "ITAcademy";
+
+
+    public static String generateAccessToken(UserDetails user) {
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setClaims((Claims) user.getAuthorities())
+                .setIssuer(jwtIssuer)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(7))) // 1 week
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public static String getUsername(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public static Collection<GrantedAuthority> getAuthorities(String token) {
+
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        Collection<?> roles = claims.get("aut", Collection.class);
+
+        if (roles == null)
+        {
+            return Collections.emptyList();
+        }
+
+        ArrayList<GrantedAuthority> authsList = new ArrayList<>(roles.size());
+
+            for (Object role : roles) {
+                authsList.add(new SimpleGrantedAuthority(role.toString()));
+            }
+
+            return Collections.unmodifiableList(authsList);
+    }
+
+    public static Date getExpirationDate(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getExpiration();
+    }
+
+    public static boolean validate(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            //logger.error("Invalid JWT signature - {}", ex.getMessage());
+        } catch (MalformedJwtException ex) {
+            //logger.error("Invalid JWT token - {}", ex.getMessage());
+        } catch (ExpiredJwtException ex) {
+            //logger.error("Expired JWT token - {}", ex.getMessage());
+        } catch (UnsupportedJwtException ex) {
+            //logger.error("Unsupported JWT token - {}", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            //logger.error("JWT claims string is empty - {}", ex.getMessage());
+        }
+        return false;
+    }
+}
