c066732fd23cec0c18cfed899a922233a6ff5145
Ilya Papruga <ilya.papruga@gmail.com>
2022-07-25 16:10:43 +0300

    add dao methods

--- EventFilmService/src/main/java/by/it_academy/jd2/EventFilmService/core/dao/api/IFilmDao.java
+++ EventFilmService/src/main/java/by/it_academy/jd2/EventFilmService/core/dao/api/IFilmDao.java
@@ -2,12 +2,18 @@ package by.it_academy.jd2.EventFilmService.core.dao.api;
 
 import by.it_academy.jd2.EventFilmService.core.entity.Film;
 
+import by.it_academy.jd2.EventFilmService.core.entity.enums.EventStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IFilmDao extends JpaRepository <Film, UUID> {
-
+    Page<Film> findByEventStatus(EventStatus status, Pageable pageable);
+    Page<Film> findByEventStatusOrAuthor(EventStatus status, String author, Pageable pageable);
 }
