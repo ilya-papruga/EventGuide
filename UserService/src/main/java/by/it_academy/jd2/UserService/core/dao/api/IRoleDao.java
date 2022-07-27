fcb925283f451e359db84527fd15bd4db63ce761
Ilya Papruga <ilya.papruga@gmail.com>
2022-07-27 23:53:59 +0300

    add roles and user to commandliner

--- UserService/src/main/java/by/it_academy/jd2/UserService/core/dao/api/IRoleDao.java
+++ UserService/src/main/java/by/it_academy/jd2/UserService/core/dao/api/IRoleDao.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IRoleDao extends JpaRepository<Role, String> {
+    Role findByName(String name);
+
 }
--- UserService/src/main/java/by/it_academy/jd2/UserService/service/DefaultUser.java
+++ UserService/src/main/java/by/it_academy/jd2/UserService/service/DefaultUser.java
@@ -1,5 +1,6 @@
 package by.it_academy.jd2.UserService.service;
 
+import by.it_academy.jd2.UserService.core.dao.api.IRoleDao;
 import by.it_academy.jd2.UserService.core.dao.api.IUserDao;
 import by.it_academy.jd2.UserService.core.entity.Role;
 import by.it_academy.jd2.UserService.core.entity.User;
@@ -9,17 +10,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 @Component
 public class DefaultUser implements CommandLineRunner {
 
 
-    private final IUserDao dao;
+    private final IRoleDao roleDao;
+    private final IUserDao userDao;
     private final PasswordEncoder encoder;
 
-    public DefaultUser(IUserDao dao, PasswordEncoder encoder) {
-        this.dao = dao;
+    public DefaultUser(IRoleDao roleDao, IUserDao userDao, PasswordEncoder encoder) {
+        this.roleDao = roleDao;
+        this.userDao = userDao;
         this.encoder = encoder;
     }
 
@@ -27,11 +31,23 @@ public class DefaultUser implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        if (dao.findByMail("admin@gmail.com") == null) {
+        if (roleDao.findByName("ROLE_ADMIN") == null) {
+
+            roleDao.save(new Role("ROLE_ADMIN"));
+
+        }
+
+        if (roleDao.findByName("ROLE_USER") == null) {
+
+            roleDao.save(new Role("ROLE_USER"));
+
+        }
+
+        if (userDao.findByMail("admin@gmail.com") == null) {
 
             User admin = new User();
             admin.setUuid(UUID.randomUUID());
-            admin.setDtCreate(LocalDateTime.now());
+            admin.setDtCreate(LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS));
             admin.setDtUpdate(admin.getDtCreate());
             admin.setMail("admin@gmail.com");
             admin.setNick("admin");
@@ -39,7 +55,22 @@ public class DefaultUser implements CommandLineRunner {
             admin.setStatus(UserStatus.ACTIVATED);
             admin.setPassword(encoder.encode("admin"));
 
-            dao.save(admin);
+            userDao.save(admin);
+        }
+
+        if (userDao.findByMail("user@gmail.com") == null) {
+
+            User user = new User();
+            user.setUuid(UUID.randomUUID());
+            user.setDtCreate(LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS));
+            user.setDtUpdate(user.getDtCreate());
+            user.setMail("user@gmail.com");
+            user.setNick("user");
+            user.setRole(List.of(new Role("ROLE_USER")));
+            user.setStatus(UserStatus.ACTIVATED);
+            user.setPassword(encoder.encode("user"));
+
+            userDao.save(user);
         }
     }
 }
