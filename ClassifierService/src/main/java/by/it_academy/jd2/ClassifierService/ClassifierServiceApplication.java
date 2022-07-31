cdac95b473aefedd268b7ef09d3008de09eda04d
Ilya Papruga <ilya.papruga@gmail.com>
2022-08-01 02:11:22 +0300

    add default locale

--- ClassifierService/src/main/java/by/it_academy/jd2/ClassifierService/ClassifierServiceApplication.java
+++ ClassifierService/src/main/java/by/it_academy/jd2/ClassifierService/ClassifierServiceApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.Locale;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaRepositories("by.it_academy.jd2.ClassifierService.core.dao.api")
 public class ClassifierServiceApplication {
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("ru"));
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(ClassifierServiceApplication.class, args);
     }
--- EventConcertService/src/main/java/by/it_academy/jd2/EventConcertService/EventConcertServiceApplication.java
+++ EventConcertService/src/main/java/by/it_academy/jd2/EventConcertService/EventConcertServiceApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.Locale;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaRepositories("by.it_academy.jd2.EventConcertService.core.dao.api")
 public class EventConcertServiceApplication {
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("ru"));
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(EventConcertServiceApplication.class, args);
     }
